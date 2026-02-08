05480c8520d9971455fe2f8d3ea0f51032f51713
Simon <175155691+Simon-Weij@users.noreply.github.com>
2026-02-08 20:05:37 +0100

    Packaged for nix

--- flake.nix
+++ flake.nix
@@ -1,7 +1,25 @@
 {
-  inputs.flakelight.url = "github:nix-community/flakelight";
-  outputs = {flakelight, ...}:
+  inputs = {
+    flakelight.url = "github:nix-community/flakelight";
+    wayland-recorder.url = "github:simon-weij/wayland-recorder";
+  };
+  outputs = {
+    flakelight,
+    wayland-recorder,
+    ...
+  }:
     flakelight ./. {
-      devShell.packages = pkgs: [pkgs.flutter];
+      package = {
+        flutter,
+        lib,
+        ...
+      }:
+        flutter.buildFlutterApplication {
+          pname = "wayland-recorder-frontend";
+          version = "1.0.0";
+          src = ./.;
+          autoPubspecLock = ./pubspec.lock;
+        };
+      devShell.packages = pkgs: [pkgs.flutter wayland-recorder.packages.${pkgs.system}.default];
     };
 }
